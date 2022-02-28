ff3128482d22a49c30b1da8bd16567da5fda7507
Sean Gallagher <stgallag@gmail.com>
2022-02-28 15:32:44 -0500

    Include us maps, add map command

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,22 @@ use avis::errors::Result;
 use clap::arg;
 
 fn main() -> Result<()> {
+    let wordcloudcommand = clap::Command::new("wordcloud")
+        .arg(arg!(--words <WORDLIST> "JSON file containing list of words to use, see example"));
+    let mapcommand = clap::Command::new("map");
     let args = clap::Command::new("avis")
-        .arg(arg!(--words <WORDLIST> "JSON file containing list of words to use, see example"))
+        .subcommand(wordcloudcommand)
+        .subcommand(mapcommand)
         .get_matches();
-    avis::visuals::wordcloud::WordCloudVisual::new(&args.value_of_t_or_exit::<PathBuf>("words"))?.start()?;
+    match args.subcommand() {
+        Some(("wordcloud", subargs)) => {
+            avis::visuals::wordcloud::WordCloudVisual::new(&subargs.value_of_t_or_exit::<PathBuf>("words"))?.start()?;
+        },
+        Some(("map", subargs)) => {
+            avis::visuals::reliefmap::main()?;
+        }
+        _ => panic!("Please choose a command")
+    }
+    
     Ok(())
 }
