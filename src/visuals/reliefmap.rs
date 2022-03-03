0485e99b3f995540eead57e48b4c80c45607291b
Sean Gallagher <stgallag@gmail.com>
2022-03-02 19:08:12 -0500

    Include county name and state name

--- src/visuals/reliefmap.rs
+++ src/visuals/reliefmap.rs
@@ -1,5 +1,6 @@
 use crate::errors::*;
 use crate::meshutil::estimate_vertex_normals;
+use anyhow::*;
 use bevy::prelude::*;
 use bevy::render::mesh::Indices;
 use bevy::render::render_resource::PrimitiveTopology;
@@ -7,12 +8,15 @@ use geo::prelude::*;
 use geo::simplifyvw::SimplifyVWPreserve;
 use geo::MultiPolygon;
 use geo::Triangle;
-use geojson::Geometry;
 use std::f32::consts::PI;
-use std::io::Read;
+use std::str::FromStr;
 
 #[derive(Component)]
-struct Map;
+struct County {
+    name: String,
+    state: String,
+    polygon: geo::Polygon<f32>,
+}
 
 pub fn main() -> Result<()> {
     App::new()
@@ -21,181 +25,175 @@ pub fn main() -> Result<()> {
         .add_plugins(DefaultPlugins)
         .add_plugin(bevy_atmosphere::AtmospherePlugin { dynamic: false })
         .add_plugin(bevy_fly_camera::FlyCameraPlugin)
-        .add_startup_system(setup)
+        .add_startup_system(setup_counties)
         .run();
     Ok(())
 }
 
-fn create_map() -> Mesh {
-    use bevy::render::{mesh::Indices, render_resource::PrimitiveTopology};
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    let (min_x, max_x, min_y, max_y, min_z, max_z) = (-0.5, 0.5, -0.1, 0.1, -0.5, 0.5);
-    let vertices = &[
-        // Top
-        ([min_x, min_y, max_z], [0., 0., 1.0], [0., 0.]),
-        ([max_x, min_y, max_z], [0., 0., 1.0], [1.0, 0.]),
-        ([max_x, max_y, max_z], [0., 0., 1.0], [1.0, 1.0]),
-        ([min_x, max_y, max_z], [0., 0., 1.0], [0., 1.0]),
-        // Bottom
-        ([min_x, max_y, min_z], [0., 0., -1.0], [1.0, 0.]),
-        ([max_x, max_y, min_z], [0., 0., -1.0], [0., 0.]),
-        ([max_x, min_y, min_z], [0., 0., -1.0], [0., 1.0]),
-        ([min_x, min_y, min_z], [0., 0., -1.0], [1.0, 1.0]),
-        // Right
-        ([max_x, min_y, min_z], [1.0, 0., 0.], [0., 0.]),
-        ([max_x, max_y, min_z], [1.0, 0., 0.], [1.0, 0.]),
-        ([max_x, max_y, max_z], [1.0, 0., 0.], [1.0, 1.0]),
-        ([max_x, min_y, max_z], [1.0, 0., 0.], [0., 1.0]),
-        // Left
-        ([min_x, min_y, max_z], [-1.0, 0., 0.], [1.0, 0.]),
-        ([min_x, max_y, max_z], [-1.0, 0., 0.], [0., 0.]),
-        ([min_x, max_y, min_z], [-1.0, 0., 0.], [0., 1.0]),
-        ([min_x, min_y, min_z], [-1.0, 0., 0.], [1.0, 1.0]),
-        // Front
-        ([max_x, max_y, min_z], [0., 1.0, 0.], [1.0, 0.]),
-        ([min_x, max_y, min_z], [0., 1.0, 0.], [0., 0.]),
-        ([min_x, max_y, max_z], [0., 1.0, 0.], [0., 1.0]),
-        ([max_x, max_y, max_z], [0., 1.0, 0.], [1.0, 1.0]),
-        // Back
-        ([max_x, min_y, max_z], [0., -1.0, 0.], [0., 0.]),
-        ([min_x, min_y, max_z], [0., -1.0, 0.], [1.0, 0.]),
-        ([min_x, min_y, min_z], [0., -1.0, 0.], [1.0, 1.0]),
-        ([max_x, min_y, min_z], [0., -1.0, 0.], [0., 1.0]),
+fn read_counties() -> Result<Vec<County>> {
+    let states = [
+        "",
+        "Alabama",
+        "Alaska",
+        "Arizona",
+        "Arkansas",
+        "California",
+        "Colorado",
+        "Connecticut",
+        "Delaware",
+        "Florida",
+        "Georgia",
+        "Hawaii",
+        "Idaho",
+        "Illinois",
+        "Indiana",
+        "Iowa",
+        "Kansas",
+        "Kentucky",
+        "Louisiana",
+        "Maine",
+        "Maryland",
+        "Massachusetts",
+        "Michigan",
+        "Minnesota",
+        "Mississippi",
+        "Missouri",
+        "Montana",
+        "Nebraska",
+        "Nevada",
+        "New Hampshire",
+        "New Jersey",
+        "New Mexico",
+        "New York",
+        "North Carolina",
+        "North Dakota",
+        "Ohio",
+        "Oklahoma",
+        "Oregon",
+        "Pennsylvania",
+        "Rhode Island",
+        "South Carolina",
+        "South Dakota",
+        "Tennessee",
+        "Texas",
+        "Utah",
+        "Vermont",
+        "Virginia",
+        "Washington",
+        "West Virginia",
+        "Wisconsin",
+        "Wyoming",
     ];
-    mesh.set_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        vertices
-            .iter()
-            .map(|&(loc, _norm, _uv)| loc)
-            .collect::<Vec<_>>(),
-    );
-    let positions: Vec<_> = vertices
-        .iter()
-        .map(|&(loc, _norm, _uv)| Vec3::from(loc))
-        .collect();
-    let indices = vec![
-        0, 1, 2, 2, 3, 0, 4, 5, 6, 6, 7, 4, 8, 9, 10, 10, 11, 8, 12, 13, 14, 14, 15, 12, 16, 17,
-        18, 18, 19, 16, 20, 21, 22, 22, 23, 20,
-    ];
-    let normals: Vec<_> = estimate_vertex_normals(&positions, &indices)
-        .unwrap()
-        .into_iter()
-        .map(|v| v.to_array())
-        .collect();
-    mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-    mesh.set_attribute(
-        Mesh::ATTRIBUTE_UV_0,
-        vertices
-            .iter()
-            .map(|&(_loc, _norm, uv)| uv)
-            .collect::<Vec<_>>(),
-    );
-    mesh.set_indices(Some(Indices::U32(indices)));
-    mesh
+
+    // This JSON is invalid UTF8, unfortunately, so we fix it on the fly, which uses more memory.
+    let shapes = std::fs::read("us-maps/geojson/county.geo.json")?;
+    let shapes = String::from_utf8_lossy(&shapes)
+        .into_owned()
+        .parse::<geojson::GeoJson>()?;
+    let mut counties = vec![];
+    for feature in geojson::FeatureCollection::try_from(shapes)?.features {
+        let name = feature
+            .property("NAMELSAD10")
+            .ok_or(anyhow!("County missing name"))?
+            .as_str()
+            .ok_or(anyhow!("County name is not a string"))?
+            .to_string();
+        let state_number = feature
+            .property("STATEFP10")
+            .ok_or(anyhow!("County missing state number"))?
+            .as_str()
+            .ok_or(anyhow!("County state number is not an integer in a string"))?
+            .parse::<usize>()?;
+        let state = states
+            .get(state_number)
+            .unwrap_or(&"Other Territory")
+            .to_string();
+        if feature.geometry.is_none() {
+            continue;
+        }
+        let polygons: geo::Geometry<f32> = feature.geometry.unwrap().try_into()?;
+        let polygons = match polygons {
+            geo::Geometry::MultiPolygon(mp) => mp.0,
+            geo::Geometry::Polygon(p) => vec![p],
+            _ => vec![],
+        };
+        for polygon in polygons {
+            counties.push(County {
+                name: name.clone(),
+                state: state.clone(),
+                polygon,
+            })
+        }
+    }
+    Ok(counties)
 }
 
-/// sets up a scene with textured entities
-fn setup(
+/// Set up a series of meshes to represent the counties
+fn setup_counties(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let material_handle = materials.add(StandardMaterial {
-        base_color: Color::DARK_GREEN,
-        alpha_mode: AlphaMode::Blend,
-        unlit: false,
-        ..Default::default()
-    });
-    let map_rotation = Quat::from_axis_angle(Vec3::Y, PI / 4.);
-
-    // This JSON is invalid UTF8, unfortunately, so we fix it on the fly, which uses more memory.
-    let shapes = std::fs::read("us-maps/geojson/county.geo.json")
-        .expect("Couldn't find the county geojson file. Please makes sure you initialized the git submodule");
-    let shapes = String::from_utf8_lossy(&shapes)
-        .parse::<geojson::GeoJson>()
-        .expect("Couldn't read the county-level US map geo-json");
-    geojson::quick_collection(&shapes)
-        .expect("Failed to interpret map json as a collection")
-        .into_iter()
-        .flat_map(|geom: geo::Geometry<f32> | match geom {
-            geo::Geometry::MultiPolygon(mp) => mp.0,
-            geo::Geometry::Polygon(p) => vec![p],
-            _ => vec![]
-        })//geom.try_into().ok())
-        //.flat_map(|mpoly: geo::Polygon<f32>| mpoly.into_iter())
+    for county in read_counties().expect("Failed to read counties") {
+        // We could simplify the polygons to accelerate rendering, but it seems unnecessary.
         //.map(|poly| poly.simplifyvw_preserve(&0.0001))
-        .map(|poly| {
-            eprint!(".");
-            let altitude = rand::random::<f32>() / 50.0;
-            let tris = delaunator::triangulate(
-                &poly
-                    .exterior()
-                    .points()
-                    .map(|p| delaunator::Point {
-                        x: -p.x() as f64,
-                        y: p.y() as f64,
-                    })
-                    .collect::<Vec<_>>(),
-            );
+        let ref poly = county.polygon;
+        eprint!(".");
+        let altitude = rand::random::<f32>() / 50.0;
+        let tris = delaunator::triangulate(
+            &poly
+                .exterior()
+                .points()
+                .map(|p| delaunator::Point {
+                    x: -p.x() as f64,
+                    y: p.y() as f64,
+                })
+                .collect::<Vec<_>>(),
+        );
 
-            let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-            let positions = poly
-                .exterior()
-                .points()
-                .map(|p| [-p.x() / 10., altitude, p.y() / 10.])
-                .collect::<Vec<_>>();
-            let uv0 = poly
-                .exterior()
-                .points()
-                .map(|p| [0.5+(p.x() / 360.0), 0.5+(p.y() / 360.0)])
-                .collect::<Vec<_>>();
-            let indices = tris
-                .triangles
-                .into_iter()
-                .map(|u| u as u32)
-                .collect::<Vec<_>>();
-            // let normals = (0..positions.len() / 3).flat_map(|_| [0.,1.,0.]).collect::<Vec<_>>();
-            let normals = estimate_vertex_normals(
-                &positions.iter().map(|v| Vec3::from(*v)).collect::<Vec<_>>(),
-                &indices,
-            )
-            .expect("Couldn't guess normals")
-            .into_iter()
-            .map(|v| v.to_array())
+        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        let positions = poly
+            .exterior()
+            .points()
+            .map(|p| [-p.x() / 10., altitude, p.y() / 10.])
             .collect::<Vec<_>>();
-            //mesh.set_attribute(Mesh::ATTRIBUTE_COLOR, vec![[1.0, 1.0, 0.0, 1.0]; positions.len()]);
-            mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
-            mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-            mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uv0);
-            mesh.set_indices(Some(Indices::U32(indices)));
-            mesh
-        })
-        .for_each(|mesh| {
-            let material = materials.add(StandardMaterial {
-                base_color: Color::rgb(rand::random(), rand::random(), rand::random()),
-                alpha_mode: AlphaMode::Blend,
-                unlit: false,
-                ..Default::default()
-            });
-            commands.spawn().insert(Map).insert_bundle(PbrBundle {
-                mesh: meshes.add(mesh),
-                transform: Transform::from_rotation(map_rotation),
-                material,
-                ..Default::default()
-            });
+        let uv0 = poly
+            .exterior()
+            .points()
+            .map(|p| [0.5 + (p.x() / 360.0), 0.5 + (p.y() / 360.0)])
+            .collect::<Vec<_>>();
+        let indices = tris
+            .triangles
+            .into_iter()
+            .map(|u| u as u32)
+            .collect::<Vec<_>>();
+        let normals = estimate_vertex_normals(
+            &positions.iter().map(|v| Vec3::from(*v)).collect::<Vec<_>>(),
+            &indices,
+        )
+        .expect("Couldn't guess normals")
+        .into_iter()
+        .map(|v| v.to_array())
+        .collect::<Vec<_>>();
+        mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+        mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+        mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uv0);
+        mesh.set_indices(Some(Indices::U32(indices)));
+
+        let material = materials.add(StandardMaterial {
+            base_color: Color::rgb(rand::random(), rand::random(), rand::random()),
+            alpha_mode: AlphaMode::Blend,
+            unlit: false,
+            ..Default::default()
         });
+        commands.spawn().insert(county).insert_bundle(PbrBundle {
+            mesh: meshes.add(mesh),
+            material,
+            ..Default::default()
+        });
+    }
 
-    let mesh = create_map();
-
-    // Map
-    commands.spawn().insert(Map).insert_bundle(PbrBundle {
-        mesh: meshes.add(mesh),
-        transform: Transform::from_rotation(map_rotation),
-        material: material_handle.clone(),
-        ..Default::default()
-    });
-
-    // floor
+    // Floor, if there is any meaning to it.
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(bevy::prelude::shape::Plane { size: 100. }.into()),
         material: materials.add(StandardMaterial {
@@ -224,7 +222,7 @@ fn setup(
         ..Default::default()
     });
 
-    // camera
+    // Flyable camera
     commands
         .spawn_bundle(PerspectiveCameraBundle {
             transform: Transform::from_xyz(1.0, 2.0, 4.0)
